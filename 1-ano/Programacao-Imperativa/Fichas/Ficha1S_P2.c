471b4a4da9373a50d6cb0233a79a7691edf9b595
José Martins <102693585+NullaSec@users.noreply.github.com>
2023-02-26 03:08:37 +0000

    Add files via upload

--- 1-ano/Programacao-Imperativa/Fichas/Ficha1S_P2.c
+++ 1-ano/Programacao-Imperativa/Fichas/Ficha1S_P2.c
@@ -3,18 +3,67 @@
 
 // 3. Programas Iterativos - Escrever programas
 // a)
-void quadrados (int contador)
+void quadrado (int n)
 {
-    int i = 0;
-    int j = 0;
-    while (i < contador)
+    int i, j;
+
+    for (i = 0; i < n; i++)
     {
-        while (j < contador)
+        for (j = 0; j < n; j++)
         {
             putchar('#');
-            j += 1;
         }
         putchar('\n');
-        i += 1;
     }
+}
+
+// b)
+void xadrez (int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        if (i % 2 == 0){printf("#_#_#");}
+        else {printf("_#_#_");}
+
+        putchar('\n');
+    }
+}
+
+// c)
+void triangulo1 (int n) // Vertical
+{
+    int i, j, x, y, k = 1;
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < k; j++)
+        {
+            putchar('#');
+        }
+        putchar('\n');
+        k++;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (x = 0; x < y - 1; x++)
+        {
+            putchar('#');
+        }
+        putchar('\n');
+        y--;
+    }
+}
+
+
+
+int main()
+{
+    quadrado(5);
+    xadrez(5);
+    triangulo1(5);
+
+    return 0;
 }
