f9d895799c98b42a406eb6371a401d531c6d8e82
José Martins <102693585+NullaSec@users.noreply.github.com>
2024-12-14 11:33:15 +0000

    Nova resolução do apelidos

--- 2-ano/Lab-Algoritmia2/Treino 1/apelidos.py
+++ 2-ano/Lab-Algoritmia2/Treino 1/apelidos.py
@@ -1,18 +1,28 @@
-# APELIDOS
-'''
-Defina uma função que, dada uma lista de nomes de pessoas, devolva essa lista ordenada
-por ordem crescente do número de apelidos (todos menos o primeiro nome). No caso de pessoas com o mesmo número de apelidos,
-devem ser listadas por ordem lexicográfica do nome completo.
-'''
 
-# return lista de apelidos ordenados por tamanho
-# caso dois apelidos tenham o mesmo tamanho ordenar alfabeticamente
+# Defina uma função que, dada uma lista de nomes de pessoas, devolva essa lista ordenada 
+# por ordem crescente do número de apelidos (todos menos o primeiro nome). 
+# No caso de pessoas com o mesmo número de apelidos,devem ser listadas por ordem lexicográfica 
+# do nome completo.
 
-nomes = ['Jose Carlos Bacelar Almeida', 'Maria Joao Frade', 'Jose Bernardo Barros', 'Jorge Manuel Matos Sousa Pinto', 'Manuel Alcino Pereira Cunha', 'Xico Esperto']
+nomes = ["Jose Carlos Bacelar Almeida",
+        "Maria Joao Frade",
+        "Jose Bernardo Barros",
+        "Jorge Manuel Matos Sousa Pinto",
+        "Manuel Alcino Pereira Cunha",
+        "Xico Esperto"]
+
+lista_nomes = []
+
+def num_apelidos(nome):
+    apelidos = nome.split()
+    n_a = len(apelidos) - 1
+    return n_a
 
 def apelidos(nomes):
-    num_apelidos = sorted(nomes, key = lambda apelido: (len(apelido.split()), apelido))
+    lista_nomes = sorted(nomes, key = lambda nome: (num_apelidos(nome), nome))
+    return lista_nomes
+        
 
-    return num_apelidos
+        
 
-apelidos(nomes)
+    
