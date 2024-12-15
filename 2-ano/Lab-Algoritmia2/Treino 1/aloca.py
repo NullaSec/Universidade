c7338af8c5161ff1ba7194cd3922eb523afe08bf
José Martins <102693585+NullaSec@users.noreply.github.com>
2024-12-15 20:10:06 +0000

    Nova versão do Aloca

--- 2-ano/Lab-Algoritmia2/Treino 1/aloca.py
+++ 2-ano/Lab-Algoritmia2/Treino 1/aloca.py
@@ -1,32 +1,23 @@
-# ALOCA
-
-"""
-Implemente uma função que dado um dicionário com as preferências dos alunos
-por projectos (para cada aluno uma lista de identificadores de projecto, 
-por ordem de preferência), aloca esses alunos aos projectos. A alocação
-é feita por ordem de número de aluno, e cada projecto só pode ser feito
-por um aluno. A função deve devolver a lista com os alunos que não ficaram
-alocados a nenhum projecto, ordenada por ordem de número de aluno.
-"""
-
-# Ordem numerica
-# 1 projeto por aluno
-# Return lista dos sem projeto ordenada numericamente
+# Implemente uma função que dado um dicionário com as preferências dos alunos
+# por projectos (para cada aluno uma lista de identificadores de projecto, 
+# por ordem de preferência), aloca esses alunos aos projectos. A alocação
+# é feita por ordem de número de aluno, e cada projecto só pode ser feito
+# por um aluno. A função deve devolver a lista com os alunos que não ficaram
+# alocados a nenhum projecto, ordenada por ordem de número de aluno.
 
 prefs = {10885:[1,5],40000:[5],10000:[1,2]}
 
 def aloca(prefs):
     sorted_dic = sorted(prefs.items())
-    semprojeto = [aluno[0] for aluno in sorted_dic]
+    semprojeto = [aluno[0] for aluno in sorted_dic] # [0] é a key
     alocados = set()
 
     for aluno, projetos in sorted_dic:
         for projeto in projetos:
             if projeto not in alocados:
+
                 semprojeto.remove(aluno)
                 alocados.add(projeto)
                 break
 
     return semprojeto
-
-aloca(prefs)
